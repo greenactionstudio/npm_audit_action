3107a6a1d7f0f5e1bae61277c36385387554ffa0
Lior Frank <liorfrank@mail.tau.ac.il>
2022-12-20 12:33:01 +0200

    action

---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+const { spawnSync } = require('child_process');
+
+async function run() {
+    try {
+        // Run npm audit with the requested severity
+        const auditResult = spawnSync('npm', ['audit', `-audit-level=${core.getInput('severity')}`, '--json']);
+
+        // If there are vulnerabilities with the requested severity or higher, exit code will be 1
+        if (auditResult.status === 1) {
+            core.setFailed(`Found vulnerability with "${core.getInput('severity')}" severity or higher`);
+        }
+
+        // Parse the output as JSON
+        const auditJson = JSON.parse(auditResult.stdout);
+
+        // Check for vulnerabilities with a certain string in the title
+        const forbiddenString = core.getInput('title');
+        if (forbiddenString !== '') {
+            for (const vulnerability in auditJson.vulnerabilities) {
+                if (auditJson.vulnerabilities[vulnerability].via[0].title.includes(forbiddenString)) {
+                    core.setFailed(`Found vulnerability with "${forbiddenString}" in the title`);
+                }
+            }
+        }
+        console.log("Here is the full npm audit report:")
+        console.dir(auditJson);
+    }
+
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
+
